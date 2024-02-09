243fe99ed016568e6cc4362e53f1bd0bd47da19d
paulforrs <paulforros@gmail.com>
2024-02-09 11:31:34 +0800

    cors

--- src/index.js
+++ src/index.js
@@ -20,12 +20,12 @@ app.use(authRouter)
 
 app.use(cors());
 // Enable CORS for all routes
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     next();
-//   });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173/');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
 
 module.exports =  app.listen(port, ()=>{
